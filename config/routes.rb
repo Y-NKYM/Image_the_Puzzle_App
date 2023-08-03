f1a9816f33b8f1615a8750f4f5d3edf7e17fb6b7
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-08-03 12:02:26 +0000

    [Add] Controllers/Actions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,51 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'post_comments/index'
+    get 'post_comments/show'
+    get 'post_comments/edit'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'tags/index'
+  end
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'bookmarks/index'
+  end
+  namespace :public do
+    get 'searches/index'
+  end
+  namespace :public do
+    get 'tags/index'
+  end
+  namespace :public do
+    get 'post_comments/index'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 顧客用
