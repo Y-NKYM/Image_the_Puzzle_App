ee7aef001069392c5199d44ec5585ef23f75a449
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-08-19 00:20:21 +0000

    [Update] パズルレスポンシブ完了、turbolinks無効化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+//import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -17,5 +17,5 @@ import "../stylesheets/application";
 import "croppie";
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
